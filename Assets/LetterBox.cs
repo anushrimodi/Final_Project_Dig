5453088875a3736cbc492cabec4d7f192a0a3b3f
anushrimodi <anmodi@davidson.edu>
2025-10-17 11:03:38 -0400

    v1

---
+++ Assets/LetterBox.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+public class LetterBox : MonoBehaviour
+{
+    public TMP_Text letterBoxText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
